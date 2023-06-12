2e701d37e6639273a28047ecc87234ced80b16cb
inyong shim <siy415@naver.com>
2023-06-12 20:53:58 +0900

    api call update

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 from flask import Flask, request, json, jsonify
+import os, sys
+sys.path.append(os.path.abspath(".\\ChinJiIn\\chinjiin"))
+import ChinJiIn.chinjiin.chinjiin as chinjiin 
 
 # https://github.com/tjwjdgus12/ChinJiIn
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
-@app.route("/test", methods=['GET'])
+@app.route("/fixer", methods=['GET'])
 def test():
     sentence = request.args.get('sentence')
-    print("data: {}".format(sentence))
+    words = sentence.split(' ')
+    fixed_words = []
+    print("data: {}".format(words))
 
+    for w in words:
+        fixed_represents = chinjiin.word_fixer.more_fix(w)
+        fixed_word = chinjiin.fix(w)
+        print("fixed word: {}".format(fixed_word))
+        print("represents: {}".format(fixed_represents))
+        fixed_words.append(fixed_word)
+    print("data: {}".format(fixed_words))
+
+    fixed_sentence = str.join(' ', fixed_words)
+    print(fixed_represents)
     response = {
         "result": "ok",
-        "fixed": sentence,
+        "fixed": fixed_sentence,
     }
 
-    return jsonify(response)
+    return fixed_sentence
 
 
 if __name__ == "__main__":
--- fixer.py
+++ fixer.py
@@ -5,5 +5,16 @@ import ChinJiIn.chinjiin.chinjiin as chinjiin
 
 
 
-print(chinjiin.word_fixer.more_fix('오늘ㅇㅡㄴ'))
-print(chinjiin.word_fixer.more_fix('정발'))
+sentence = "다시 만난 세게"
+words = sentence.split(' ')
+fixed_words = []
+print(words)
+
+for w in words:
+    fixed_represents = chinjiin.word_fixer.more_fix(w)
+    fixed_word = chinjiin.fix(w)
+    print(fixed_word)
+    print(fixed_represents)
+    fixed_words.append(fixed_word)
+
+print(fixed_words)
